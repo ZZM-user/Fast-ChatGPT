06f4f070012dec03f879e4572a78d13354cd52d2
17602 <z2657272578@gmail.com>
2023-03-05 14:25:09 +0800

    修改绑定host

--- main.py
+++ main.py
@@ -33,4 +33,4 @@ async def talk(
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8459, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8459, reload=True)
