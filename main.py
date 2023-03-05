f1b6b3c4c85b8a7aecd153259455321e43ae92c0
17602 <z2657272578@gmail.com>
2023-03-05 18:53:25 +0800

    新增BingChat

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ async def talk(
 ):
     item.prompt = item.prompt.strip()
     print(item)
-    
+
     if item.prompt.startswith("bing"):
         prompt = item.prompt.removeprefix("bing")
+        print(prompt)
         answer = bingChat.talk(prompt)
     else:
         answer = chatGPT.talk(item.sender, item.prompt)
